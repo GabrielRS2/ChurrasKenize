1028685e342ddb66badc4c987c4ba5d32d6bd6e2
PipoqueiroFeliz <matheus.fcarvalho2001@gmail.com>
2022-07-06 11:01:07 -0300

    fixed: Responsividade DashboardUser

--- src/Pages/Events/index.jsx
+++ src/Pages/Events/index.jsx
@@ -3,6 +3,7 @@ import background from "./../../Assets/Background-User.jpg";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import { ThemeInput } from "../../Styles/ThemeInput";
 
 export const EventsPage = () => {
   const schema = yup.object().shape({
@@ -28,86 +29,89 @@ export const EventsPage = () => {
   return (
     <Container>
       <div className="entirePage">
-      <div className="bodyEventPage">
-        <form onSubmit={handleSubmit(onSubmitFunction)}>
-          <p>Solicite um evento</p>
-          <label>
-            Data {<span className="error">{errors.date?.message}</span>}
-          </label>
-          <input
-            name="date"
-            placeholder="Digite a data do evento"
-            {...register("date")}
-          />
+        <div className="bodyEventPage">
+          <form onSubmit={handleSubmit(onSubmitFunction)}>
+            <p>Solicite um evento</p>
 
-          <label>
-            Horário{<span className="error">{errors.time?.message}</span>}
-          </label>
-          <input
-            name="time"
-            placeholder="Digite o horário do evento"
-            {...register("time")}
-          />
+            <ThemeInput
+              label="Data"
+              name="date"
+              placeHolder="Digite a data do evento"
+              error={errors.date?.message}
+              register={register}
+            />
 
-          <label>
-            Duração{<span className="error">{errors.duration?.message}</span>}
-          </label>
-          <input
-            name="duration"
-            placeholder="Digite a duração do evento"
-            {...register("duration")}
-          />
-          <label>Estado</label>
-          <select name="state" {...register("state")}>
-            <option value="">Selecione o estado</option>
-            <option value="AC">Acre</option>
-            <option value="AL">Alagoas</option>
-            <option value="AP">Amapá</option>
-            <option value="AM">Amazonas</option>
-            <option value="BA">Bahia</option>
-            <option value="CE">Ceará</option>
-            <option value="DF">Distrito Federal</option>
-            <option value="ES">Espírito Santo</option>
-            <option value="GO">Goiás</option>
-            <option value="MA">Maranhão</option>
-            <option value="MT">Mato Grosso</option>
-            <option value="MS">Mato Grosso do Sul</option>
-            <option value="MG">Minas Gerais</option>
-            <option value="PA">Pará</option>
-            <option value="PB">Paraíba</option>
-            <option value="PR">Paraná</option>
-            <option value="PE">Pernambuco</option>
-            <option value="PI">Piauí</option>
-            <option value="RJ">Rio de Janeiro</option>
-            <option value="RN">Rio Grande do Norte</option>
-            <option value="RS">Rio Grande do Sul</option>
-            <option value="RO">Rondônia</option>
-            <option value="RR">Roraima</option>
-            <option value="SC">Santa Catarina</option>
-            <option value="SP">São Paulo</option>
-            <option value="SE">Sergipe</option>
-            <option value="TO">Tocantins</option>
-            <option value="EX">Estrangeiro</option>
-          </select>
-          <label>
-            Cidade{<span className="error">{errors.city?.message}</span>}
-          </label>
-          <input
-            name="city"
-            placeholder="Digite a cidade do evento"
-            {...register("city")}
-          />
-          <label>
-            Combo{<span className="combo">{errors.combo?.message}</span>}
-          </label>
-          <input
-            name="combo"
-            placeholder="Digite o combo desejado"
-            {...register("combo")}
-          />
-          <button type="submit">Solicitar evento</button>
-        </form>
-      </div>
+            <ThemeInput
+              label="Horário"
+              name="time"
+              placeHolder="Digite o horário do evento"
+              error={errors.time?.message}
+              register={register}
+            />
+            <ThemeInput
+              label="Duração"
+              name="duration"
+              placeHolder="Digite a duração do evento"
+              error={errors.duration?.message}
+              register={register}
+            />
+            <label>Estado</label>
+            <select name="state" {...register("state")}>
+              <option value="">Selecione o estado</option>
+              <option value="AC">Acre</option>
+              <option value="AL">Alagoas</option>
+              <option value="AP">Amapá</option>
+              <option value="AM">Amazonas</option>
+              <option value="BA">Bahia</option>
+              <option value="CE">Ceará</option>
+              <option value="DF">Distrito Federal</option>
+              <option value="ES">Espírito Santo</option>
+              <option value="GO">Goiás</option>
+              <option value="MA">Maranhão</option>
+              <option value="MT">Mato Grosso</option>
+              <option value="MS">Mato Grosso do Sul</option>
+              <option value="MG">Minas Gerais</option>
+              <option value="PA">Pará</option>
+              <option value="PB">Paraíba</option>
+              <option value="PR">Paraná</option>
+              <option value="PE">Pernambuco</option>
+              <option value="PI">Piauí</option>
+              <option value="RJ">Rio de Janeiro</option>
+              <option value="RN">Rio Grande do Norte</option>
+              <option value="RS">Rio Grande do Sul</option>
+              <option value="RO">Rondônia</option>
+              <option value="RR">Roraima</option>
+              <option value="SC">Santa Catarina</option>
+              <option value="SP">São Paulo</option>
+              <option value="SE">Sergipe</option>
+              <option value="TO">Tocantins</option>
+              <option value="EX">Estrangeiro</option>
+            </select>
+            <ThemeInput
+              label="Cidade"
+              name="city"
+              placeHolder="Digite a cidade do evento"
+              error={errors.city?.message}
+              register={register}
+            />
+            <label>
+              Cidade{<span className="error">{errors.city?.message}</span>}
+            </label>
+            <input
+              name="city"
+              placeholder="Digite a cidade do evento"
+              {...register("city")}
+            />
+            <ThemeInput
+              label="Combo"
+              name="combo"
+              placeHolder="Digite o combo desejado"
+              error={errors.combo?.message}
+              register={register}
+            />
+            <button type="submit">Solicitar evento</button>
+          </form>
+        </div>
         <div className="infoProfile">
           <div className="profile">
             <p>Meu perfil</p>
@@ -116,7 +120,7 @@ export const EventsPage = () => {
             <p>Eventos</p>
           </div>
         </div>
-        </div>
+      </div>
       <img src={background} alt="background" />
     </Container>
   );
--- src/Pages/Events/style.js
+++ src/Pages/Events/style.js
@@ -1,127 +1,127 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-background: #212529;
-img{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--grey-1);
+  img {
     width: 100%;
     height: 100%;
     z-index: 0;
     margin: 0;
     padding: 0;
     display: none;
-}
+  }
 
-form{
+  form {
     display: flex;
     gap: 12px;
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
-    width: 296px;
+    width: 300px;
     height: auto;
     background-color: #212529;
     border-radius: 6px;
-    width: 369px;
+    /* width: 369px; */
     padding: 20px;
-}
+    margin: 25px auto;
+  }
 
-button{
+  button {
     width: 60%;
     height: 48px;
-    background-color: #FF0909 ;
+    background-color: #ff0909;
     border-radius: 4px;
-    font-family: 'Poppins', sans-serif;
+    font-family: "Poppins", sans-serif;
     text-align: center;
     font-size: 11px;
     color: white;
-}
+  }
 
-select{
-    width:60%;
-    height: 30px;
-}
-
-input{
+  select {
     width: 60%;
     height: 30px;
-    margin-bottom: 15px;
-    border-radius: 4px;
-    outline: none;
-}
+  }
 
-p{
+  p {
     color: white;
-    font-family: 'Poppins', sans-serif;
+    font-family: "Poppins", sans-serif;
     font-size: 22px;
-}
+  }
 
-a,label{
+  a,
+  label {
     color: white;
-    font-family: 'Poppins', sans-serif;
+    font-family: "Poppins", sans-serif;
     font-size: 11px;
-}
+  }
 
-div{
+  div {
     z-index: 1;
-}
+  }
 
-.profile{
-    width: 369px;
-    height: 233px;
-    background-color: #212529;
-    position: relative;
-    border-radius: 4px;
-}
-
-.events{
-    width: 369px;
-    height: 233px;
-    background-color: #212529;
-    position: relative;
-    border-radius: 4px;
-}
-
-.bodyEventPage{
-    display: flex;
-    flex-direction: row-reverse;
-}
-
-
-
-@media screen and (min-width: 680px) {
-    form{
-    width: 410px;
-    height: auto;
-    }   
-    img{
-    display: block;
-    }
-    div{
-        position: absolute;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .bodyEventPage{
-        right: 0vw;
-    }
-
-    .profile{
-        margin-bottom: 60px;
-        width: 400px;
-    }
-    .events{
-        width: 400px;
-    }
-
-    .infoProfile{
+  .infoProfile {
+    gap: 15px;
     display: flex;
     flex-direction: column;
-    margin-left: 600px;
-}
   }
-`
+
+  .profile {
+    width: 300px;
+    height: 200px;
+    background-color: #212529;
+    position: relative;
+    border-radius: 4px;
+  }
+
+  .events {
+    width: 300px;
+    height: 200px;
+    background-color: #212529;
+    position: relative;
+    border-radius: 4px;
+  }
+
+  .bodyEventPage {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+
+  @media screen and (min-width: 680px) {
+    form {
+      width: 410px;
+      height: auto;
+    }
+    img {
+      display: block;
+    }
+    .entirePage,
+    .bodyEventPage,
+    .infoProfile {
+      position: absolute;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .bodyEventPage {
+      right: 0vw;
+    }
+
+    .profile {
+      margin-bottom: 60px;
+      width: 400px;
+    }
+    .events {
+      width: 400px;
+    }
+
+    .infoProfile {
+      display: flex;
+      flex-direction: column;
+      margin-left: 600px;
+    }
+  }
+`;
