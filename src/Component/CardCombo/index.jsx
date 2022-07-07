580518eb26b3dc1bb3691b45d74d81016b53bea8
Leonardo Carnot <leocarnot8@gmail.com>
2022-07-07 15:14:30 -0300

    feat: aparece descrição combo no cart

--- src/Component/CardCombo/index.jsx
+++ src/Component/CardCombo/index.jsx
@@ -1,35 +1,56 @@
-import { Container } from "./style.js"
-import { useContext } from "react";
-import { UserContext } from "../../Providers/User.js"
+import { Container } from "./style.js";
+import { useState } from "react";
 
-export const CardCombo = () =>{
-    const {user} = useContext(UserContext)
-    
-    return(
-        <Container>
-            <div className="card__container">
-                {user.map(user=>{
-                return <div className="card" key={user.id}>
-                    <div className="card__info">
-                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM-9BhAquXWRan3TnaL_ndnjAG0pXWkbxCkg&usqp=CAU' alt="foto do usuário"/>
-                        <div>
-                        <p>{user.name}</p>
-                        <p>{user.city} - {user.state}</p>
-                        </div>
-                    </div>
+export const CardCombo = ({ combo }) => {
+  const [detail, setDetail] = useState(false);
 
-                    <div className="card__combo">
-                        <h2>{user.name}</h2>
-                        <img src={user.img} alt="foto do card"/>
-                    </div>
+  function detailOn() {
+    setDetail(true);
+  }
 
-                    <div className="card__button">
-                        <button className="active">Solicitar Acesso</button>
-                        <button >Detalhes</button>
-                    </div>
+  function detailOff() {
+    setDetail(false);
+  }
+
+  return (
+    <Container>
+      {detail ? (
+        <>
+        <p>{combo.description}</p>
+        <button onClick={detailOff}>X</button>
+        </>
+      ) : (
+        <div className="card__container">
+          {combo.map((combo) => {
+            return (
+              <div className="card" key={combo.id}>
+                <div className="card__info">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM-9BhAquXWRan3TnaL_ndnjAG0pXWkbxCkg&usqp=CAU"
+                    alt="foto do usuário"
+                  />
+                  <div>
+                    <p>{combo.name}</p>
+                    <p>
+                      {combo.city} - {combo.state}
+                    </p>
+                  </div>
                 </div>
-                })}
-            </div>
-        </Container>
-    )
-}
+
+                <div className="card__combo">
+                  <h2>{combo.name}</h2>
+                  <img src={combo.img} alt="foto do card" />
+                </div>
+
+                <div className="card__button">
+                  <button className="active">Solicitar Acesso</button>
+                  <button onClick={detailOn}>Detalhes</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </Container>
+  );
+};
