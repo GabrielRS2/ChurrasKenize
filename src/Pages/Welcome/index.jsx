06c8faf86abb8bb7701807011566c6d51bef104f
Leonardo Carnot <leocarnot8@gmail.com>
2022-07-08 11:00:45 -0300

    fix: fixing linking pages

--- src/Pages/Welcome/index.jsx
+++ src/Pages/Welcome/index.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import { Container } from './style';
-import Logo from '../../Assets/Logo.svg'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Container } from "./style";
+import Logo from "../../Assets/Logo.svg";
+import { Link } from "react-router-dom";
+import { Header } from "../../Component/Header";
+import { Footer } from "../../Component/Footer";
 
 export const Welcome = () => {
-  return(
-    <Container>
-        <div className='left'>
-            <img src={Logo} alt="logo do site" />
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className="left">
+          <img src={Logo} alt="logo do site" />
         </div>
 
-        <div className='right'>
-            <h1>Olá, Seja Bem Vindo(a)!</h1>
-            <p>Pensamos na praticidade para o seu churrasco, encontre o churrasqueiro ideal e mais próximo de você!</p>
-            <button><Link to="/home">Saiba mais...</Link></button>
+        <div className="right">
+          <h1>Olá, Seja Bem Vindo(a)!</h1>
+          <p>
+            Pensamos na praticidade para o seu churrasco, encontre o
+            churrasqueiro ideal e mais próximo de você!
+          </p>
+          <button>
+            <Link to="/">Saiba mais...</Link>
+          </button>
         </div>
-    </Container>
-  )
-}
-
+      </Container>
+      <Footer />
+    </>
+  );
+};
