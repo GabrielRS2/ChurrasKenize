ae3ab040f08dd2daecd9bd210501a9c3a18f8617
PipoqueiroFeliz <matheus.fcarvalho2001@gmail.com>
2022-07-08 16:33:53 -0300

    fix: componentização de login e api

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,88 +1,14 @@
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
-import { Link, useHistory } from "react-router-dom";
+import { FormsLogin } from "../../Component/FormsLogin";
+import { Logo } from "../../Component/Logo";
 import Login from "./../../Assets/Login-image.svg";
-import Logo from "./../../Assets/Logo.svg";
 import { Container } from "./style";
-import { ThemeInput } from "../../Styles/ThemeInput";
-import ThemeButton from "../../Styles/ThemeButton";
-import { useContext } from "react";
-import { ApiContext } from "../../Providers/Api";
-import { toast } from "react-toastify";
 
 export const LoginPage = () => {
-  const history = useHistory();
-
-  const { loginUser } = useContext(ApiContext);
-
-  const schema = yup.object().shape({
-    email: yup.string().email("Email inválido").required("Campo Obrigatório"),
-    password: yup
-      .string()
-      .min(8, "Mínimo de 8 dígitos")
-      .required("Campo Obrigatório"),
-  });
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const onSubmitFunction = async (data) => {
-    const res = await loginUser(data);
-    if (res.name !== "AxiosError") {
-      localStorage.setItem(
-        "@churraskenzie:token",
-        JSON.stringify(res.data.accessToken)
-      );
-      localStorage.setItem(
-        "@churraskenzie:userId",
-        JSON.stringify(res.data.user.id)
-      );
-      history.push("/dashboardUser");
-    } else {
-      toast.error("Usuario ou senha inválido");
-    }
-  };
-
   return (
     <Container>
       <div className="background">
-        <img src={Logo} alt="Logo" className="logoImage" />
-        <form onSubmit={handleSubmit(onSubmitFunction)}>
-          <p>Login</p>
-
-          <ThemeInput
-            label="Email"
-            name="email"
-            placeholder="Digite seu email"
-            error={errors.email?.message}
-            register={register}
-          />
-
-          <ThemeInput
-            label="Senha"
-            name="password"
-            placeholder="Digite sua senha"
-            type="password"
-            error={errors.password?.message}
-            register={register}
-          />
-
-          <ThemeButton schema="var(--red-2)" large type="submit">
-            Entrar
-          </ThemeButton>
-          <span>Ainda não possui uma conta?</span>
-          <Link to="/register">
-            <ThemeButton schema="var(--grey-2)" large>
-              Cadastre-se
-            </ThemeButton>
-          </Link>
-        </form>
+        <Logo />
+        <FormsLogin />
       </div>
       <img src={Login} alt="login background" className="loginImage" />
     </Container>
