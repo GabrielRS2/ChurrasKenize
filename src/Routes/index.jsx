4cbe18ec8a3636c7a1241789f9721be8ed2e33ee
Micaías Silva <micaiasilvaop@gmail.com>
2022-07-09 13:55:24 -0300

    feat: /dashboardBbc acessível

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,9 +1,10 @@
 import { Switch, Route } from "react-router-dom";
-import { Welcome } from '../Pages/Welcome/index'
+import { Welcome } from "../Pages/Welcome/index";
 import { LoginPage } from "../Pages/Login";
 import { RegisterPage } from "../Pages/Register";
 import { EventsPage } from "../Pages/Events";
 import { DashboardUser } from "../Pages/DashboardUser";
+import DashboardBbc from "../Pages/DashboardBbc";
 
 export const Routes = () => {
   return (
@@ -11,21 +12,21 @@ export const Routes = () => {
       <Route exact path="/">
         <Welcome />
       </Route>
-      <Route exact path="/home"> 
-      </Route>
-      <Route exact path="/dashboardBbc"> 
+      <Route exact path="/home"></Route>
+      <Route exact path="/dashboardBbc">
+        <DashboardBbc />
       </Route>
       <Route exact path="/dashboardUser">
-        <DashboardUser/> 
+        <DashboardUser />
       </Route>
       <Route exact path="/login">
-        <LoginPage/>
+        <LoginPage />
       </Route>
       <Route exact path="/register">
-        <RegisterPage/>
+        <RegisterPage />
       </Route>
       <Route exact path="/events">
-        <EventsPage/>
+        <EventsPage />
       </Route>
     </Switch>
   );
