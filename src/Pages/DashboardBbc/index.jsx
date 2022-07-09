b8a6e2e2d681c99bf4920b212392fae4cecbfe92
Micaías Silva <micaiasilvaop@gmail.com>
2022-07-09 13:56:08 -0300

    feat: DashboardBbc - Estrutura básica e estilização razoável

--- src/Pages/DashboardBbc/index.jsx
+++ src/Pages/DashboardBbc/index.jsx
@@ -1,29 +1,36 @@
+import CardBbc from "../../Component/CardBbc";
+import CardProfileBbc from "../../Component/CardProfileBbc";
+import ComboForm from "../../Component/ComboForm";
 import { Header } from "../../Component/Header";
 import { Container } from "./style";
 
 function DashboardBbc() {
   return (
-    <div className="dashbbc_container">
-      <Header />
-      <Container>
-        <main>
-          <section className="">
-            <h2>Pedidos Recebidos</h2>
-          </section>
-          <section>
-            <h2>Combos de Churrasco</h2>
-          </section>
-        </main>
-        <aside>
-          <div>
-            <h2>Meu perfil</h2>
-          </div>
-          <div>
-            <h2>Agenda</h2>
-          </div>
-        </aside>
-      </Container>
-    </div>
+    <Container>
+      <div className="dashbbc_container">
+        <Header />
+        <div className="wrapper">
+          <main className="combo-lists">
+            <section className="bbc_cards_container">
+              <h2>Pedidos Recebidos</h2>
+              <CardBbc />
+              <h2>Combos de Churrasco</h2>
+              <CardBbc />
+            </section>
+          </main>
+          <aside className="bbc_user_info">
+            <div className="bbc_cards_container">
+              <h2>Meu perfil</h2>
+              <CardProfileBbc />
+            </div>
+            <div className="bbc_cards_container">
+              <h2>Criar Combo</h2>
+              <ComboForm />
+            </div>
+          </aside>
+        </div>
+      </div>
+    </Container>
   );
 }
 
