ca43f2aefcb430438452fb55cf1b0cd12ff03358
PipoqueiroFeliz <matheus.fcarvalho2001@gmail.com>
2022-07-11 13:43:20 -0300

    fixed: filtros por preço

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,12 +5,13 @@ import { MdSearch } from "react-icons/md";
 import { CardCombo } from "../../Component/CardCombo";
 import { useEffect, useState } from "react";
 import api from "../../Services";
+import { Hr } from "../../Component/Hr";
 
 export const Home = () => {
   const [combos, setCombos] = useState([]);
-  const [value, setValue] = useState('')
-  const [price, setPrice] = useState()
-  const [quantity, setQuantity] = useState()
+  const [value, setValue] = useState("");
+  const [price, setPrice] = useState();
+  const [quantity, setQuantity] = useState();
 
   useEffect(() => {
     api.get("/combos").then((response) => {
@@ -18,18 +19,22 @@ export const Home = () => {
     });
   }, []);
 
-  const filteredName = combos.filter(combo=>{
-    return combo.combo.toLowerCase().includes(value.toLowerCase()) || combo.name.toLowerCase().includes(value.toLowerCase()) || combo.city.toLowerCase().includes(value.toLowerCase())
-  });
-  
-  const filteredPrice = combos.filter(combo=>{
-    return combo.price < price;
+  const filteredName = combos.filter((combo) => {
+    return (
+      combo.combo.toLowerCase().includes(value.toLowerCase()) ||
+      combo.name.toLowerCase().includes(value.toLowerCase()) ||
+      combo.city.toLowerCase().includes(value.toLowerCase())
+    );
   });
 
-  const filteredQuantity = combos.filter(combo=>{
+  const filteredPrice = combos.filter((combo) => {
+    return price < 0 ? combo.price > 800 : combo.price < price;
+  });
+
+  const filteredQuantity = combos.filter((combo) => {
     return combo.quantity <= quantity;
   });
-  
+
   return (
     <>
       <Header />
@@ -41,24 +46,38 @@ export const Home = () => {
 
               <select value={price} onChange={(e) => setPrice(e.target.value)}>
                 <option>Preço</option>
-                <option value='400'>R$ 400</option>
-                <option value='300'>R$ 300</option>
-                <option value='200'>R$ 200</option>
+                <option value="-1">partir de R$ 800</option>
+                <option value="800">até R$ 800</option>
+                <option value="500">até R$ 500</option>
+                <option value="200">até R$ 200</option>
               </select>
 
-              <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+              <select
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+              >
                 <option>Quantidade</option>
-                <option value='20'>20 pessoas</option>
-                <option value='10'>10 pessoas</option>
+                <option value="20">20 pessoas</option>
+                <option value="10">10 pessoas</option>
               </select>
             </div>
 
             <div className="header__search">
-              <MdSearch className="icon"/>
-              <input type="text" placeholder="Faça sua pesquisa..." value={value} onChange={(e) => setValue(e.target.value)}/>
+              <MdSearch className="icon" />
+              <input
+                type="text"
+                placeholder="Faça sua pesquisa..."
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
             </div>
           </header>
-          <CardCombo filteredName={filteredName} filteredPrice={filteredPrice} filteredQuantity={filteredQuantity}/>
+          <Hr />
+          <CardCombo
+            filteredName={filteredName}
+            filteredPrice={filteredPrice}
+            filteredQuantity={filteredQuantity}
+          />
         </div>
       </Container>
       <Footer />
