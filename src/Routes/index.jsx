ff728e35f545f42e9f418ac7d93c3a9244994c0d
GabrielRS2 <gabers20000@gmail.com>
2022-07-11 10:01:31 -0300

    fix: rotas

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,9 +1,7 @@
 import { Switch, Route } from "react-router-dom";
-import { Welcome } from "../Pages/Welcome/index";
 import { LoginPage } from "../Pages/Login";
 import { RegisterPage } from "../Pages/Register";
 import { EventsPage } from "../Pages/Events";
-import { DashboardUser } from "../Pages/DashboardUser";
 import DashboardBbc from "../Pages/DashboardBbc";
 import { Home } from "../Pages/Home";
 import { AboutUs } from "../Pages/AboutUs";
@@ -18,8 +16,6 @@ export const Routes = () => {
       <Route exact path="/dashboardBbc">
         <DashboardBbc />
       </Route>
-      <Route exact path="/dashboardUser">
-        <DashboardUser />
       <Route exact path="/aboutUs">
         <AboutUs/>
       </Route>
