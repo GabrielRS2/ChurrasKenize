5d4c41888680a12c1bbe16888c57e71b0e0d0d76
Daniel Josias <daniieljosiaslima@gmail.com>
2022-07-11 17:57:26 -0300

    feat: create combo

--- src/Component/ModalCreateCombo/index.jsx
+++ src/Component/ModalCreateCombo/index.jsx
@@ -1,15 +1,58 @@
-import { useForm } from "react-hook-form";
 import ThemeButton from "../../Styles/ThemeButton";
 import { ThemeInput } from "../../Styles/ThemeInput";
 import { ThemeTextArea } from "../../Styles/ThemeTextArea";
 import { Container } from "./style";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { toast } from 'react-toastify'
+import { useContext } from "react";
+import { UserContext } from "../../Providers/User";
+import api from "../../Services";
+
 
 export const ModalCreateCombo = ({ handleCloseModal }) => {
-  const onSubmitFunction = (data) => {
-    console.log(data);
-  };
+  const { user } = useContext(UserContext);
 
-  const { register, handleSubmit } = useForm();
+  const formSchema = yup.object().shape({
+    combo: yup.string().required("Campo obrigatório"),
+    price: yup.string().required("Campo obrigatório"),
+    quantity: yup.string().required("Campo obrigatório"),
+    duration: yup.string().required("Campo obrigatório"),
+    img: yup.string().url("URL inválida!").required("Campo obrigatório"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+  
+  const onSubmitFunction = (data) => {
+    const token = JSON.parse(localStorage.getItem('@churraskenzie:token'))
+    data["userId"] = user.id
+    data["name"] = user.name
+
+    api.post("/combos",data,{
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    })
+    .then((_)=>{
+      handleCloseModal()
+    })
+    .catch(error =>{
+      if(error){
+        toast.error('Falha ao criar o combo!')
+      }else{
+        toast.success('Combo criado com sucesso!')
+        
+      }
+    })
+   
+  }
 
   return (
     <>
@@ -19,38 +62,49 @@ export const ModalCreateCombo = ({ handleCloseModal }) => {
         <button onClick={handleCloseModal} className="closeModal">X</button>
       </div>
       <form onSubmit={handleSubmit(onSubmitFunction)}>
+      <ThemeInput
+        label="Nome do combo"
+        name="combo"
+        placeholder="Digite seu combo"
+        error={errors.combo?.message}
+        register={register}
+      />
+
+      <ThemeInput
+        label="Preço"
+        name="price"
+        placeholder="Digite seu price"
+        error={errors.price?.message}
+        register={register}
+      />
+
+      <ThemeInput
+        label="Quantidade"
+        name="quantity"
+        placeholder="Digite a quantidade de pessoas"
+        type="text"
+        error={errors.quantity?.message}
+        register={register}
+      />
+      <ThemeInput
+        label="Duração"
+        name="duration"
+        placeholder="Digite a duração"
+        type="text"
+        error={errors.duration?.message}
+        register={register}
+      />
+
         <ThemeInput
-          label="Preço"
-          name="price"
-          placeholder="Preço do combo"
-          register={register}
-        />
-        <ThemeInput
-          label="Duração"
-          name="duration"
-          placeholder="Tempo de evento para combo"
-          register={register}
-        />
-        <ThemeInput
-          label="Nome do combo"
-          name="combo"
-          placeholder="Digite o nome do combo"
-          register={register}
-        />
-        <ThemeInput
-          label="Quantidade de pessoas"
-          name="quantity"
-          placeholder="Digite a quantidade de pessoas"
-          register={register}
-        />
-        <ThemeTextArea
-          label="Descrição"
-          name="description"
-          placeholder="Descrição do combo"
-          register={register}
-        />
+        label="Avatar"
+        name="img"
+        placeholder="Digite a url da imagem"
+        type="text"
+        error={errors.avatar?.message}
+        register={register}
+      />
         <ThemeButton schema="var(--red-2)" medium type="submit">
-                Enviar combo
+               Criar combo
               </ThemeButton>
       </form>
       </Container>
--- src/Pages/DashboardBbc/index.jsx
+++ src/Pages/DashboardBbc/index.jsx
@@ -5,24 +5,66 @@ import CardProfileBbc from "../../Component/CardProfileBbc";
 import { ComboListItem } from "../../Component/ComboListItem";
 import { Footer } from "../../Component/Footer";
 import { Header } from "../../Component/Header";
+import { ModalCreateCombo } from "../../Component/ModalCreateCombo";
 import { Schedule } from "../../Component/Schedule";
 import { UserContext } from "../../Providers/User";
 import api from "../../Services";
 import ThemeButton from "../../Styles/ThemeButton";
 import { Container, ContentContainer } from "./style";
+import Modal from 'react-modal'
 
 function DashboardBbc() {
   const [combos, setCombos] = useState([]);
   const { user } = useContext(UserContext);
+  
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function handleOpenModal() {
+    setIsOpen(true);
+  }
+
+  function handleCloseModal() {
+    setIsOpen(false);
+  }
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      background: "transparent",
+      border: "none",
+    },
+    overlay: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: "rgba(18, 18, 20, 0.5)",
+    },
+  };
 
   useEffect(() => {
     api.get(`/combos?userId=${user.id}`).then((res) => setCombos(res.data));
   }, []);
 
   console.log(combos);
+  
 
   return (
     <Container>
+       <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={handleCloseModal}
+          style={customStyles}
+        >
+          <ModalCreateCombo handleCloseModal={handleCloseModal} />
+        </Modal>
+        
       <Header />
       <ContentContainer>
         <main>
@@ -35,7 +77,8 @@ function DashboardBbc() {
           <div className="eventListBbc">
             <div className="headerComboList">
               <h2>Combos</h2>
-              <ThemeButton schema="#000000">Criar Combo</ThemeButton>
+              <ThemeButton schema="#000000" handleClick={handleOpenModal}>Criar Combo</ThemeButton>
+            
             </div>
             <ul className="ComboList">
               {combos?.map((combo) => {
